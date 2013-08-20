2381ffa71680a78b69692489c4f5163e4712a53d
JayCarney <jason.jay.carney@gmail.com>
2013-08-20 11:13:19 +0930

    added documentation comment

--- getYouTubeId.php
+++ getYouTubeId.php
@@ -1,4 +1,18 @@
 <?php
+/**
+ * youTubeId
+ * 
+ * Created by Jason Carney (DashMedia.com.au) for use
+ * as a simple output modifier for MODX, all credit for
+ * RegEx String goes to the community at StackOverflow.com
+ *
+ * @package youTubeId
+ */
+/**
+ * @var string $input   - youtube URL to video
+ * 
+ * @returns string      - youTube ID
+ */
 $ytUrl = $input;
 if(isset($ytUrl) && !empty($ytUrl)){
     $pattern = '#^(?:https?://)?(?:www\.)?(?:youtu\.be/|youtube\.com(?:/embed/|/v/|/watch\?v=|/watch\?.+&v=))([\w-]{11})(?:.+)?$#x';
